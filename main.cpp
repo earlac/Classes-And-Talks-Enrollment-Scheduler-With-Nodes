c60606bec07dc07ad8ec55b984d2f94b81157af1
Brenda Badilla Rodríguez <brendabbr2@estudiantec.com>
2021-09-03 23:49:22 -0600

    Co-authored-by: earlac <earlac@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
-//Archivo main del proyecto
-//Hola Earl está escribiendo esto :Dklhghklghg
-//
+/*
+Instituto Tecnológico de Costa Rica, Estructuras de Datos
+Proyecto 1: Gestión de actividades curriculares
+Profesora: Lorena Valerio 
+Estudiantes: Earl Areck Alvarado, 2020
+*/
+
+//Hola buenas tardes, mi nombre es Russel, y soy un guía explorador 
