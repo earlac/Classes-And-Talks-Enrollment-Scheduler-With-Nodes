e795af281a83ed310e65cd5de883c5c9ec7a3528
Brenda Badilla Rodríguez <brendabbr2@estudiantec.com>
2021-09-03 23:50:57 -0600

    Documentación

--- main.cpp
+++ main.cpp
@@ -4,5 +4,3 @@ Proyecto 1: Gestión de actividades curriculares
 Profesora: Lorena Valerio 
 Estudiantes: Earl Areck Alvarado, 2020
 */
-
-//Hola buenas tardes, mi nombre es Russel, y soy un guía explorador 
