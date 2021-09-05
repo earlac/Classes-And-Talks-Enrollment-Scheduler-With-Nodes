0bc07dbdf5414cdb6e482a7219853955181db081
Brenda Badilla Rodríguez <brendabbr2@estudiantec.com>
2021-09-04 22:14:56 -0600

    Struct Administrador

--- main.cpp
+++ main.cpp
@@ -4,3 +4,20 @@ Proyecto 1: Gestión de actividades curriculares
 Profesora: Lorena Valerio 
 Estudiantes: Earl Areck Alvarado, 2020
 */
+#include <iostream>
+
+using namespace std;
+
+struct administrador {//Doubled linked list
+    string usuario;
+    string contrasenna;
+    administrador *sigAdmin, *antAdmin;
+
+    administrador(string usuario, string contrasenna){
+        usuario = usuario;
+        contrasenna = contrasenna;
+        sigAdmin = NULL;
+        antAdmin = NULL;
+    }
+} *ListaAdministradores;
+
