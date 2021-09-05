3a39ce053180a7543b011c40dc1a62fe5e939a89
earlalvaradoc <earlareck1@gmail.com>
2021-09-05 00:00:28 -0600

    Estructuras listas
    Co-authored-by: Brenda Badilla Rodríguez <brendabbr2@estudiantec.cr>

--- main.cpp
+++ main.cpp
@@ -11,14 +11,181 @@ using namespace std;
 struct administrador {
     string usuario;
     string contrasenna;
+    
     administrador *sigAdmin;
 
-    administrador(string usuario, string contrasenna){
-        usuario = usuario;
-        contrasenna = contrasenna;
+    administrador(string nUsuario, string nContrasenna){
+        usuario = nUsuario;
+        contrasenna = nContrasenna;
+        
         sigAdmin = NULL;
     }
-} *ListaAdministradores;
+} *listaAdministradores;
+
+struct profesor{
+    string nombreProf;
+    int cedulaProf;
+
+    profesor *sigProf, *antProf;
+    struct enlazarGrupo *gruposProfAux;
+
+    profesor(string nNombreProf, int nCedulaProf){
+        nombreProf= nNombreProf;
+        cedulaProf= nCedulaProf;
+
+        sigProf= NULL;
+        antProf= NULL;
+        gruposProfAux= NULL;
+        
+    }
+
+} *listaProfesores;
+
+struct grupo{
+    grupo *sigGrupo;
+    int numGrupo;    
+    struct curso *cursoActual;
+    
+    
+    grupo(int nNumGrupo){
+        numGrupo= nNumGrupo;
+        
+        sigGrupo= NULL;
+    }
+} *listaGrupos;
+
+struct enlazarGrupo{
+
+    enlazarGrupo *sigEn;
+    struct grupo *enlaceGrupo;
+    
+    enlazarGrupo(){
+        sigEn= NULL;
+        enlaceGrupo= NULL;
+    }
+    
+};
+
+struct semestre{
+    struct curso *listaCursos;
+    int numSemestre;
+    int anno;
+
+    semestre(int nSemestre, int nAnno){
+        nSemestre= nSemestre;
+        anno= nAnno;
+        
+        listaCursos= NULL;
+    }
+} *listaSemestres;
+
+struct enlaceCurso{
+    string codigoCurso;
+    struct curso *sigCurso;
+
+    enlaceCurso(string nCodigoCurso){
+        codigoCurso= nCodigoCurso;
+        sigCurso= NULL;
+    }
+
+};
+
+struct curso{
+    string nombre;
+    string codigo;
+    
+    curso *sigCurso;
+    struct grupo *grupoCursando;
+    curso(string nNombre, string nCodigo){
+        nombre= nNombre;
+        codigo= nCodigo;
+
+        grupoCursando=NULL;
 
 
+    }
+} *listaCursos;
 
+struct evaluacion{
+    
+    struct tarea *sigTarea;
+    struct examen *sigExamen;
+    struct proyecto *sigProyecto;
+    struct gira *sigGira;
+    
+    evaluacion(){
+
+        sigTarea= NULL;
+        sigExamen = NULL;
+        sigProyecto= NULL;
+        sigGira= NULL;
+        
+    }
+};
+
+struct tarea{
+    int fechaEntrega;
+    string idActividad;
+    bool entregado = false;
+
+    tarea *sigTarea;
+    
+    tarea(int nFechaEntrega, string nIdActividad, bool nEntregado){
+        fechaEntrega= nFechaEntrega;
+        idActividad= nIdActividad;
+        entregado= nEntregado;
+        
+        sigTarea= NULL;
+
+    }
+};
+
+struct examen{
+    int fechaEntrega;
+    string idActividad;
+    bool entregado = false;
+    
+    examen *sigExamen;
+
+    examen(int nfechaEntrega, string nIdAct, bool nEntregado){
+        fechaEntrega = nfechaEntrega;
+        idActividad = nIdAct;
+        entregado = nEntregado;
+        
+        sigExamen = NULL;
+    }
+};
+
+struct proyecto{
+    int fechaEntrega;
+    string idActividad;
+    bool entregado = false;
+
+    proyecto *sigProyecto;
+
+    proyecto(int nFechaEntrega, string nIdAct, bool nEntregado){
+        fechaEntrega = nFechaEntrega;
+        idActividad = nIdAct;
+        entregado = nEntregado;
+
+        sigProyecto= NULL;
+    }
+};
+
+struct gira{
+    
+    int fechaEntrega;
+    string idActividad;
+    bool entregado = false;
+
+    gira *sigGira;
+    
+    gira(int nFechaEntrega, string nIdAct, bool nEntregado){
+        fechaEntrega = nFechaEntrega;
+        idActividad = nIdAct;
+        entregado = nEntregado;
+
+        sigGira = NULL;
+
+    }
+};
